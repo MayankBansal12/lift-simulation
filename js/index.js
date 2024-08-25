195c49ff8993414220ed4a371e9c63ae6adb8601
Mayank Bansal <mayankbansal125@gmail.com>
2024-08-26 00:11:27 +0530

    add basic ui for lift, floor, input

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
 
-
+let floorEl = document.getElementById("floor")
+let liftEl = document.getElementById("lift")
 
 function generate(e) {
     e.preventDefault()
-    console.log("generate")
+    let floorVal = floorEl.value;
+    let liftVal = liftEl.value;
+
+    if (!floorVal || !liftVal) {
+        alert("floor and lift can't be zero")
+        return;
+    }
+
+    
+
 }
